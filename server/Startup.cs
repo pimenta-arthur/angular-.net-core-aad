46767d6079c30813c8e07b5c8869af12a40f71de
Pimenta, Arthur <APimenta@wabtec.com>
2018-10-02 11:17:45 -0300

    fix imports

--- server/Startup.cs
+++ server/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace dotnet_core_aad
 {
